3d4a08151a70fbe4aaf03c7462bf76981b278b99
lazarevvk <112278468+lazarevvk@users.noreply.github.com>
2023-07-30 21:50:19 +0300

    Update datamart_query.sql

--- datamart_query.sql
+++ datamart_query.sql
@@ -23,3 +23,17 @@ SELECT user_id, recency, frequency, monetary_value
 FROM analysis.dm_rfm_segments
 ORDER BY user_id
 LIMIT 10;
+
+-- Первые 10 полученных строк по user_id, recency, frequency и monetary value:
+
+0	3	1	1
+1	2	2	2
+2	3	3	3
+3	3	3	4
+4	3	2	4
+5	2	1	1
+6	5	4	4
+7	1	5	4
+8	2	5	4
+9	2	3	4
+
